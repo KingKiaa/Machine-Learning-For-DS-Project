801b21d0f48f766b925a09729b9da190f4d3e855
KingKiaa <120366695+KingKiaa@users.noreply.github.com>
2023-04-24 00:49:15 -0400

    Add files via upload

---
+++ FinalProject_LinearSVC-2.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Machine Learning for Data Science
+#Name = 'Kia Aalaei' Project = 'Binary Rice Classification' Dataset = 'https://www.kaggle.com/datasets/seymasa/rice-dataset-gonenjasmine'
+#SVM (Linear SVC) - Poly Features added
+
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import PolynomialFeatures
+
+
+# In[2]:
+
+
+import seaborn as sns
+
+
+# In[3]:
+
+
+rice = sns.load_dataset('RiceGonenandJasmine')
+
+
+# In[4]:
+
+
+#Features is x and lables are y 
+
+X = rice.drop(['id','Class'], axis = 1)
+X.head()
+
+
+# In[5]:
+
+
+poly = PolynomialFeatures(2,3)
+poly.fit(X)
+Z = poly.transform(X)
+print(Z)
+
+
+# In[6]:
+
+
+y = rice['Class'] 
+y.head()
+
+
+# In[7]:
+
+
+from sklearn.model_selection import train_test_split 
+Z_train, Z_test, y_train, y_test = train_test_split( Z, y, test_size = 0.33) 
+#67% of dataset to training and 33% to testing 
+#Splitting dataset into training and testing
+
+
+# In[8]:
+
+
+#linear SVC model (SVM)
+lsvc = LinearSVC(dual = False)
+print(lsvc)
+
+
+# In[9]:
+
+
+#fitting model on training data
+#training score can be checked
+lsvc.fit(Z_train,y_train)
+score = lsvc.score(Z_train,y_train)
+print("Training score: ", score)
+
+
+# In[10]:
+
+
+#can also use validation method to check training score
+cv_score = cross_val_score(lsvc, Z_train, y_train, cv=10)
+print("CV average score: ", cv_score.mean())
+
+
+# In[11]:
+
+
+#Now I can predict the test data using model
+ypred = lsvc.predict(Z_test)
+confusionmatrix = confusion_matrix(y_test, ypred)
+sns.heatmap(confusionmatrix, square=True, annot=True, cbar=False, cmap='YlGnBu') #flag, YlGnBu, jet)
+plt.xlabel('predicted value')
+plt.ylabel('true value');
+print(confusionmatrix)
+
+
+# In[12]:
+
+
+cr = classification_report(y_test, ypred)
+print(cr)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
